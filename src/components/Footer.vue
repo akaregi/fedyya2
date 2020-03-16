a0cc8356e4fc8ff7d92e2244d47e3a075176becc
akaregi <akg.tachibana@gmail.com>
2020-03-16 09:13:23 +0900

    Edited footer
    Signed-off-by: akaregi <akg.tachibana@gmail.com>

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,10 +4,6 @@
       <i>fedyya</i> by AKANE AKAGI.
     </p>
     <p class="license">
-      Source codes are licensed under
-      <a href="https://opensource.org/licenses/BSD-2-Clause">BSD 2-Clause</a>.
-    </p>
-    <p class="license">
       This site is licensed under
       <a
         href="https://creativecommons.org/licenses/by-nd/4.0/legalcode"
@@ -28,7 +24,7 @@ export default class Footer extends Vue { }
   background: none;
 }
 
-.license {
+p {
   margin: 0 !important;
 }
 </style>
