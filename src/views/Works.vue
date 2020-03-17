368ac885e6ba7f9d6d2546a6a21aaa8d863be7db
akaregi <akg.tachibana@gmail.com>
2020-03-18 01:51:47 +0900

    Update works
    Signed-off-by: akaregi <akg.tachibana@gmail.com>

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -2,14 +2,24 @@
   <div class="works">
     <TitleHero title="Works" subtitle="有耶無耶な何かの紹介" />
 
+    <h3>Lianiis Min: 月生まれたちの言葉（サイト作成）</h3>
+    <figure>
+      <img src="../assets/lianiis.png" alt="Lianiis Min's page">
+      <figcaption class="has-text-grey">頑張って作りました</figcaption>
+    </figure>
+
+    <p>フルスクラッチで書いたサイトです。<i><a href="https://www.lianiis.com/">Lianiis Min</a></i> とは猫が話す言葉をイメージして作られた人工言語だそうです。</p>
+
+    <hr>
+
     <h3><a href="https://fedyya.wordpress.com">fedyya BLOG</a>: 三日坊主のブログ</h3>
-    <p>名前の通りブログでございます。滅多に書かない。以前のサイト（<code>blog.fedyya.net</code>）は死にました。</p>
+    <p>名前の通りブログでございます。滅多に書かない。以前のサイト（blog.fedyya.net）は死にました。</p>
 
     <h3><a href="https://lunaris.fedyya.net">fedyya LUNARIS</a>: ヒュムノスの詩倉庫</h3>
-    <p>某ゲームの架空言語「ヒュムノス」で紡いだ詩の倉庫でございます。えらく数が多い。</p>
+    <p>某ゲームの架空言語「ヒュムノス」で紡いだ詩の倉庫です。<a href="https://github.com/akaregi/Lunaris">GitHub</a> にすべてのソースコードが転がっています。</p>
 
     <h3><a href="https://rhei.fedyya.net">fedyya RHEI</a>: 納屋</h3>
-    <p>納屋です。</p>
+    <p>納屋 。</p>
 
     <hr>
 
@@ -20,6 +30,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+
 import TitleHero from '@/components/TitleHero.vue'
 
 @Component({ components: { TitleHero } })
