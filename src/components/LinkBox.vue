912888975aa81d3d5898cf84bace41ff416e394a
akaregi <akg.tachibana@gmail.com>
2020-03-18 01:25:55 +0900

    Added Links
    Signed-off-by: akaregi <akg.tachibana@gmail.com>

---
+++ src/components/LinkBox.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="box">
+      <a v-bind:href="url">
+        <h4>{{ title }}</h4>
+      <p class="has-text-grey-light">{{ lead }}</p>
+      </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class LinkBox extends Vue {
+  @Prop({ type: String, required: true, default: 'Untitled' })
+  title!: string
+
+  @Prop({ type: String, required: true, default: 'No lead' })
+  lead!: string
+
+  @Prop({ type: String, required: false, default: '' })
+  url!: string
+}
+</script>
+
+<style lang="scss" scoped>
+@mixin newBoxColor($x, $y, $z, $hoverX: 66, $hoverY: 185, $hoverZ: 131) {
+  box-shadow: 0 .5em 1em -.1em rgba($x, $y, $z, .2), 0 0 0 1px rgba($x, $y, $z, .02);
+  border: 1px solid rgba($x, $y, $z, .1);
+
+  &:hover {
+    box-shadow: 0 .5em 1em -.1em rgba($hoverX, $hoverY, $hoverZ, .2), 0 0 0 1px rgba($hoverX, $hoverY, $hoverZ, .02);
+    border: 1px solid rgba($hoverX, $hoverY, $hoverZ, 1);
+  }
+}
+
+a {
+  display: block;
+
+  padding: 1.25rem;
+}
+
+.box {
+  transition: .5s;
+
+  padding: 0;
+
+  @include newBoxColor(100, 100, 100);
+}
+
+.is-twitter {
+  @include newBoxColor(29, 161, 242);
+}
+
+.is-mastodon {
+  @include newBoxColor(43, 144, 217);
+}
+
+.is-scrapbox {
+  @include newBoxColor(6, 183, 50);
+}
+</style>
--- src/views/Links.vue
+++ src/views/Links.vue
@@ -3,20 +3,84 @@
     <TitleHero title="Links" subtitle="外部サイトへのリンク集" />
 
     <h3>あかれぎ関連</h3>
+    <LinkBox
+      class="is-twitter"
+      title="Twitter"
+      lead="あかれぎのツイッターアカウントです。"
+      url="https://twitter.com/akgmoegi"
+    />
 
-    <h3>それ以外</h3>
+    <LinkBox
+      class="is-mastodon"
+      title="Mastodon"
+      lead="いつもいるのはこっちの方。"
+      url="https://felesitas.cloud/@akane"
+    />
 
+    <hr>
+
+    <h3>いぇーい</h3>
+
+    <b-notification>
+      <p>🔗 リンク先の方へ： 上のソーシャルメディア的な色付けをご所望でしたらご連絡ください。</p>
+    </b-notification>
+
+    <b-notification>
+      <p>括弧内は作者名です（敬称略）。</p>
+    </b-notification>
+
+    <h4>一般</h4>
+    <LinkBox
+      title="みことはねっと（兎月くみ）"
+      lead="スラヴ語派（？）のやべーやつのご登場だ！人たらし（自認）だけどいい人だよ"
+      url="https://mikotoha.net"
+    />
+
+    <LinkBox
+      title="Escapism（にらり）"
+      lead="お絵かきとかタロットとか色々しているらしい。書いて思ったけどよく知らない……！"
+      url="https://www.escapism.work"
+    />
+
+    <LinkBox
+      title="homepage（nagiko）"
+      lead="上のマストドンサーバ管理者のページ。足を向けて寝られない。"
+      url="https://bananagikoh.github.io/homepage/pages/mastodon-use/profile.html"
+    />
+
+    <h4>Scrapbox</h4>
+    <LinkBox
+      class="is-scrapbox"
+      title="mikura-knowledge（かるま）"
+      lead="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      url="https://scrapbox.io/mikura-knowledge/"
+    />
+
+    <LinkBox
+      class="is-scrapbox"
+      title="hakobe-public（はこべ）"
+      lead="植物のやべーやつだ！べっちだったりべっちゃんこだったり異名が多い。"
+      url="https://scrapbox.io/hakobe-public/ "
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+
+import LinkBox from '@/components/LinkBox.vue'
 import TitleHero from '@/components/TitleHero.vue'
 
-@Component({ components: { TitleHero } })
+@Component({ components: { TitleHero, LinkBox } })
 export default class Links extends Vue {
   mounted () {
     document.title = 'Links - fedyya'
   }
 }
 </script>
+
+<style lang="scss" scoped>
+hr {
+  margin: 3rem 0;
+}
+</style>
